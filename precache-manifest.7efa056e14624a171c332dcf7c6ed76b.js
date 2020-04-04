d0314312fd1a8185483697242d7340eaf69f9079
Fettoukh <medamine.fettoukh@gmail.com>
2020-04-04 16:14:18 +0200

    Updates

---
+++ precache-manifest.7efa056e14624a171c332dcf7c6ed76b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1564709c46e9ce0aafccaf699acfd773",
+    "url": "/React-Game-Of-Life/index.html"
+  },
+  {
+    "revision": "14a73e17b5a68ae0b80a",
+    "url": "/React-Game-Of-Life/static/css/main.509849da.chunk.css"
+  },
+  {
+    "revision": "a96bae0f2a8f7e277733",
+    "url": "/React-Game-Of-Life/static/js/2.c4360a5c.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/React-Game-Of-Life/static/js/2.c4360a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a73e17b5a68ae0b80a",
+    "url": "/React-Game-Of-Life/static/js/main.6b01f11d.chunk.js"
+  },
+  {
+    "revision": "ca0e28939b262954d9b4",
+    "url": "/React-Game-Of-Life/static/js/runtime-main.25b27ad1.js"
+  }
+]);
